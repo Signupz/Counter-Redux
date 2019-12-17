766f99bb2bc8252d471a39876768e14655737e61
Signupz <ludovic.mazet@gmail.com>
2019-12-17 22:33:54 +0100

    Updates

---
+++ precache-manifest.64ddd10c68cc39696bc8e68e23c8861f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6597a1a5ae478735c01ef71b21116ce1",
+    "url": "//signupz.github.io/Counter-Redux/index.html"
+  },
+  {
+    "revision": "60c444c1e044937018fe",
+    "url": "//signupz.github.io/Counter-Redux/static/css/main.56baa173.chunk.css"
+  },
+  {
+    "revision": "997af6c5819791db708a",
+    "url": "//signupz.github.io/Counter-Redux/static/js/2.d2dec7f4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "//signupz.github.io/Counter-Redux/static/js/2.d2dec7f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60c444c1e044937018fe",
+    "url": "//signupz.github.io/Counter-Redux/static/js/main.da218347.chunk.js"
+  },
+  {
+    "revision": "dbf4d39ec22c0da9fd5b",
+    "url": "//signupz.github.io/Counter-Redux/static/js/runtime-main.a0a83cdf.js"
+  }
+]);
